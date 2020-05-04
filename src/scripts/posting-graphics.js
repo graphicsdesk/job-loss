c6a8a266ec4960a9f9396ae12afcae2ec07a50f5
Jason Kao <jason.kao@columbia.edu>
2020-05-03 23:37:56 -0400

    style tweaks

--- src/scripts/posting-graphics.js
+++ src/scripts/posting-graphics.js
@@ -1,8 +1,8 @@
-import rawPostings from "../../data/postings.json";
-import { select } from "d3-selection";
-import { scaleTime, scaleLinear } from "d3-scale";
-import { line } from "d3-shape";
-import { extent } from "d3-array";
+import rawPostings from '../../data/postings.json';
+import { select } from 'd3-selection';
+import { scaleTime, scaleLinear } from 'd3-scale';
+import { line } from 'd3-shape';
+import { extent } from 'd3-array';
 
 /* Data preprocessing */
 
@@ -21,27 +21,27 @@ const HEIGHT = 400;
 
 /* Create the container and canvas */
 
-const svg = select("#postings-over-time")
-  .append("svg")
-  .attr("width", WIDTH)
-  .attr("height", HEIGHT);
+const svg = select('#postings-over-time')
+  .append('svg')
+  .attr('width', WIDTH)
+  .attr('height', HEIGHT);
 
 /* Somehow get a mapping from abstract data to coordinates on our canvas */
 
 // Scales
 const xScale = scaleTime()
-  .domain(extent(postings, (d) => d.date))
+  .domain(extent(postings, d => d.date))
   .range([0, WIDTH]);
 const yScale = scaleLinear()
-  .domain(extent(postings, (d) => d.count))
+  .domain(extent(postings, d => d.count))
   .range([HEIGHT, 0]);
 
 // Function that generates the line drawing string
 const lineFn = line()
-  .x((d) => xScale(d.date))
-  .y((d) => yScale(d.count));
+  .x(d => xScale(d.date))
+  .y(d => yScale(d.count));
 
-// Bind those coordinates to shapes
+/* TODO: AXES: https://bl.ocks.org/gordlea/27370d1eea8464b04538e6d8ced39e89 */
 
-// Draw the shapes
-svg.append("path").attr("d", lineFn(postings));
+/* Draw the shapes */
+svg.append('path').attr('d', lineFn(postings));
