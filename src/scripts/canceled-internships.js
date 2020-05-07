24f8a75ce93497d1903d029a5897bce50c66c43a
Jason Kao <jason.kao@columbia.edu>
2020-05-07 00:03:57 -0400

    clustered circles bro

--- src/scripts/canceled-internships.js
+++ src/scripts/canceled-internships.js
@@ -1,14 +1,22 @@
-import companies from '../../data/employer-industries.json';
 import { select } from 'd3-selection';
 import { scaleSqrt } from 'd3-scale';
-import { forceSimulation, forceCollide, forceX, forceY } from 'd3-force';
+import { forceSimulation, forceX, forceY } from 'd3-force';
 import { interpolateSpectral } from 'd3-scale-chromatic';
-import { extent, rollup } from 'd3-array';
+import { extent, rollup, max } from 'd3-array';
+import {quadtree} from 'd3-quadtree';
 import 'd3-transition';
 import 'd3-jetpack/essentials';
 
+import companies from '../../data/employer-industries.json';
+
+/* Some constants */
+
+const WIDTH = document.body.clientWidth;
+const HEIGHT = document.body.clientHeight;
+
 /* Data preprocessing */
 
+// Generating unique industries
 const industrySet = new Set(companies.map(item => item.industry));
 const industries = [...industrySet];
 const totalIndustry = industries.length;
@@ -30,24 +38,28 @@ for (const industry in industriesProps) {
   cumValue += prop;
 }
 
-/* radiusScale */
-
 const getSize = sizeStr => parseInt(sizeStr.replace(',', '').split(' ')[0]);
+
+/* radiusScale */
 
 const radiusScale = scaleSqrt()
   .domain(extent(companies, d => getSize(d.size)))
   .range([2, 37]);
 
-// The largest node for each cluster
-const clusters = {};
+/* Generating initial nodes */
 
 const initialRadius = 700;
 
 const companyData = companies.map(({ employer, industry, size: sizeStr }) => {
-  // TODO: add industries props cum
-  const angle = (industries.indexOf(industry) / totalIndustry) * 2 * Math.PI;
+  // Old proportion
+  let cumulativeProportion = industries.indexOf(industry) / totalIndustry;
+  // New proportion
+  cumulativeProportion = industriesPropsCum[industry];
+
+  const angle = cumulativeProportion * 2 * Math.PI;
   const size = getSize(sizeStr);
-  const d = {
+
+  return {
     employer,
     industry,
     size,
@@ -55,14 +67,7 @@ const companyData = companies.map(({ employer, industry, size: sizeStr }) => {
     y: Math.sin(angle) * initialRadius + Math.random(),
     radius: radiusScale(size),
   };
-  if (!(industry in clusters)) clusters[industry] = d;
-  return d;
 });
-
-/* Some constants */
-
-const WIDTH = document.body.clientWidth;
-const HEIGHT = document.body.clientHeight;
 
 /* Create the container and canvas */
 
@@ -73,25 +78,23 @@ const svg = select('#canceled-internships')
   .translate([WIDTH / 2, HEIGHT / 2]);
 
 /* simulation force bubbles into a place and force them not to collide */
+
 const strength = 0.03;
 const simulation = forceSimulation()
   .force('x', forceX().strength(strength))
   .force('y', forceY().strength(strength))
   .force('charlotte', cjClusterForce())
-  .force(
-    'collide',
-    forceCollide(function (d) {
-      return radiusScale(d.size) + 0.5;
-    }),
-  );
+  .force('elonMuskCollide', elonMuskCollide());
 
 /* industry color scale */
+
 function industryColorsScale(industry) {
   const t = industries.indexOf(industry) / industries.length;
   return interpolateSpectral(t);
 }
 
 /* Draw the shapes */
+
 const circle = svg
   .selectAll('circle')
   .data(companyData)
@@ -105,24 +108,26 @@ const circle = svg
   });
 
 /* feed data into simulation so for every change in time it moves it into a certain place(?)*/
+
 simulation.nodes(companyData).on('tick', ticked);
 
 function ticked() {
   circle.attr('cx', d => d.x).attr('cy', d => d.y);
 }
 
-let printedOnce = false;
+/**
+ * Cluster force
+ * Look here if confused: https://observablehq.com/@d3/clustered-bubbles
+ */
 
 function cjClusterForce() {
   const strength = 0.2;
   let nodes;
 
   function force(alpha) {
+    // Group nodes by industry, then aggregate values by calculating centroid
     const centroids = rollup(nodes, centroid, d => d.industry);
-    if (!printedOnce) {
-      // console.log(centroids.get(''));
-      printedOnce = true;
-    }
+
     const l = alpha * strength;
     for (const d of nodes) {
       const { x: cx, y: cy } = centroids.get(d.industry);
@@ -136,31 +141,62 @@ function cjClusterForce() {
   return force;
 }
 
-// Move d to be adjacent to the cluster node.
-function charlottesClusteringForce(alpha) {
-  const n = companyData.length;
+/* Collision force for spacing groups */
 
-  for (let i = 0; i < n; i++) {
-    const d = companyData[i];
-    const cluster = clusters[d.industry];
-    if (cluster === d) {
-      continue;
-    }
+function elonMuskCollide() {
+  const alpha = 0.4; // fixed for greater rigidity!
+  const padding1 = 1; // separation between same-color nodes
+  const padding2 = 17; // separation between different-color nodes
+  let nodes;
+  let maxRadius;
 
-    let dx = d.x - cluster.x;
-    let dy = d.y - cluster.y;
-    let distance = Math.sqrt(dx * dx + dy * dy);
-    let r = d.radius + cluster.radius;
-    if (distance !== r) {
-      distance = ((distance - r) / distance) * alpha;
-      d.x -= dx *= distance;
-      d.y -= dy *= distance;
-      cluster.x += dx;
-      cluster.y += dy;
+  function force() {
+    const theQuadtree = quadtree(
+      nodes,
+      d => d.x,
+      d => d.y,
+    );
+    // Black box of quadtree
+    for (const d of nodes) {
+      const r = d.radius + maxRadius;
+      const nx1 = d.x - r,
+        ny1 = d.y - r;
+      const nx2 = d.x + r,
+        ny2 = d.y + r;
+      theQuadtree.visit((q, x1, y1, x2, y2) => {
+        // Truthy if q.length undefined (if q is a leaf node)
+        if (!q.length) {
+          do {
+            // q.data is a node
+            if (q.data !== d) {
+              const r =
+                d.radius +
+                q.data.radius +
+                (d.industry === q.data.industry ? padding1 : padding2);
+              let x = d.x - q.data.x;
+              let y = d.y - q.data.y;
+              let l = Math.hypot(x, y);
+              if (l < r) {
+                l = ((l - r) / l) * alpha;
+                (d.x -= x *= l), (d.y -= y *= l);
+                (q.data.x += x), (q.data.y += y);
+              }
+            }
+          } while ((q = q.next));
+        }
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+      });
     }
   }
+
+  force.initialize = _ =>
+    (maxRadius =
+      max((nodes = _), d => d.radius) + Math.max(padding1, padding2));
+
+  return force;
 }
 
+// Calculates centroid for an array of nodes
 function centroid(nodes) {
   let x = 0;
   let y = 0;
