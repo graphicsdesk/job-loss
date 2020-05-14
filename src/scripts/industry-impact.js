d0db5e30639c039e04c4da75b61851defbdbc1ad
Jason Kao <jason.kao@columbia.edu>
2020-05-14 00:50:59 -0400

    prelim function class for industry impact

--- src/scripts/industry-impact.js
+++ src/scripts/industry-impact.js
@@ -16,162 +16,184 @@ const industryChanges = Object.keys(industryChangesRaw)
   }))
   .sort((a, b) => a.percentChange - b.percentChange);
 
-/* Some constants and containers */
+function IndustryChart(divContainer, fullLength) {
+  /* Some constants and containers */
 
-const barWidth = 36;
+  let barWidth, width;
 
-const divContainer = select('#industry-impact-container');
+  let height;
 
-/* Initiation code */
+  const margin = { left: 0, top: 10, bottom: 25 };
+  if (!fullLength) margin.left = 60;
 
-const width = barWidth * industryChanges.length;
-let height;
+  /* Initiation code */
 
-const margin = { top: 10, bottom: 25 };
+  const svg = divContainer.append('svg.industry-impact-svg');
+  const container = svg.append('g').translate([margin.left, margin.top]);
+  const barsContainer = container.append('g.bars-container');
+  const axis = container.append('g.y-axis');
 
-const svg = divContainer.append('svg').at({ width });
-const container = svg.append('g').translate([0, margin.top]);
-const barsContainer = container.append('g.bars-container');
-const axis = container.append('g.y-axis');
-
-const xScale = scaleBand()
-  .domain(industryChanges.map(d => d.industry))
-  .rangeRound([0, width])
-  .paddingInner(0.04);
-const yScale = scaleLinear().domain(
-  extent(industryChanges, d => d.percentChange),
-);
-
-const percentFormat = format('.0%');
-const axisFn = axisLeft(yScale)
-  .tickFormat(percentFormat)
-  .tickPadding(10)
-  .tickSize(-width);
-
-// Join bars to data
-const bars = barsContainer
-  .selectAll('bars')
-  .data(industryChanges)
-  // Everything that doesn't depend on the yScale can be done on enter
-  .join(enter =>
-    enter.append('g').call(g => {
-      g.append('rect').at({
-        x: d => xScale(d.industry),
-        width: xScale.bandwidth(),
-      });
-      g.append('text.bar-label-name')
-        .text(d => d.industry)
-        .at({ x: d => xScale(d.industry) + barWidth / 2 });
-      g.append('text.bar-label-percentage')
-        .text(
-          d => percentFormat(d.percentChange).replace('-', '–'), // Use en dash
-        )
-        .at({ x: d => xScale(d.industry) + barWidth / 2 });
-    }),
+  const xScale = scaleBand()
+    .domain(industryChanges.map(d => d.industry))
+    .paddingInner(0.04);
+  const yScale = scaleLinear().domain(
+    extent(industryChanges, d => d.percentChange),
   );
 
-// Store nodes for easy indexing
-const barsNodes = bars.nodes();
+  const percentFormat = format('.0%');
+  const axisFn = axisLeft(yScale).tickFormat(percentFormat).tickPadding(10);
 
-function updateGraph() {
-  const svgHeight = window.innerHeight;
-  height = svgHeight - margin.top - margin.bottom;
-  svg.at({ height: svgHeight });
-
-  // Update yScale and its dependencies
-  yScale.rangeRound([height, 0]);
-  axis.call(axisFn);
-
-  // Update bar heights and positions
-  bars.select('rect').at({
-    y: d => Math.min(yScale(0), yScale(d.percentChange)),
-    height: d => Math.abs(yScale(d.percentChange) - yScale(0)),
-  });
-
-  // Update label positions
-  bars.select('text.bar-label-name').at({
-    y: d => yScale(0) + (d.percentChange / Math.abs(d.percentChange)) * 15,
-  });
-  bars.select('text.bar-label-percentage').at({
-    y: d =>
-      yScale(d.percentChange) -
-      (d.percentChange / Math.abs(d.percentChange)) * 15,
-  });
-}
-
-let highlightedBarIndex = null;
-function highlightBar(index, forever) {
-  if (index < 0 || index >= barsNodes.length) {
-    return;
-  }
-  const oldNode = barsNodes[highlightedBarIndex];
-  const newNode = barsNodes[index];
-  if (highlightedBarIndex !== index) {
-    oldNode && oldNode.classList.remove('highlight-bar-group');
-    newNode.classList.add('highlight-bar-group');
-    highlightedBarIndex = index;
-  }
-
-  if (forever) {
-    newNode.classList.add('highlight-bar-group-forever');
-  }
-}
-
-function highlightIndustriesForever(industries) {
-  industryChanges.forEach(({ industry }, i) => {
-    if (industries.includes(industry)) highlightBar(i, true);
-  });
-}
-
-// Horizontal scroll callback to highlight middle bar and stick axis
-function onScroll(scrollDistance) {
-  highlightBar(Math.round(scrollDistance / barWidth));
-}
-
-// Parses data-industries from paragraph blocks
-const paragraphs = Array.from(
-  document.querySelectorAll('#industry-content p'),
-  p => ({
-    node: p,
-    industries: p.getAttribute('data-industries').split(/;\s?/),
-  }),
-);
-function positionText() {
-  paragraphs.forEach(({ node, industries }) => {
-    const xAvg = industries.reduce(
-      (avg, industry) => avg + xScale(industry) / industries.length,
-      0,
+  // Join bars to data
+  const bars = barsContainer
+    .selectAll('bars')
+    .data(industryChanges)
+    // Everything that doesn't depend on the yScale can be done on enter
+    .join(enter =>
+      enter.append('g').call(g => {
+        g.append('rect');
+        g.append('text.bar-label-name').text(d => d.industry);
+        g.append('text.bar-label-percentage').text(
+          d => percentFormat(d.percentChange).replace('-', '–'), // Use en dash
+        );
+      }),
     );
-    const pctAvg = industries.reduce(
-      (avg, industry) => avg + industryChangesRaw[industry] / industries.length,
-      0,
+
+  // Store nodes for easy indexing
+  const barsNodes = bars.nodes();
+
+  this.updateGraph = function () {
+    const svgWidth = fullLength ? 36 * industryChanges.length : 600;
+    width = svgWidth - margin.left;
+    const svgHeight = fullLength ? window.innerHeight : 400;
+    height = svgHeight - margin.top - margin.bottom;
+    svg.at({ width: svgWidth, height: svgHeight });
+
+    barWidth = width / industryChanges.length;
+
+    // Update yScale and its dependencies
+    xScale.rangeRound([0, width]);
+    yScale.rangeRound([height, 0]);
+    axis.call(axisFn.tickSize(-width));
+
+    // Update bar heights and positions
+    bars.select('rect').at({
+      x: d => xScale(d.industry),
+      y: d => Math.min(yScale(0), yScale(d.percentChange)),
+      width: xScale.bandwidth(),
+      height: d => Math.abs(yScale(d.percentChange) - yScale(0)),
+    });
+
+    // Update label positions
+    bars.select('text.bar-label-name').at({
+      x: d => xScale(d.industry) + barWidth / 2,
+      y: d => yScale(0) + (d.percentChange / Math.abs(d.percentChange)) * 15,
+    });
+    bars.select('text.bar-label-percentage').at({
+      x: d => xScale(d.industry) + barWidth / 2,
+      y: d =>
+        yScale(d.percentChange) -
+        (d.percentChange / Math.abs(d.percentChange)) * 15,
+    });
+  };
+
+  let highlightedBarIndex = null;
+  this.highlightBar = function (index, forever) {
+    if (index < 0 || index >= barsNodes.length) {
+      return;
+    }
+    const oldNode = barsNodes[highlightedBarIndex];
+    const newNode = barsNodes[index];
+    if (highlightedBarIndex !== index) {
+      oldNode && oldNode.classList.remove('highlight-bar-group');
+      newNode.classList.add('highlight-bar-group');
+      highlightedBarIndex = index;
+    }
+
+    if (forever) {
+      newNode.classList.add('highlight-bar-group-forever');
+    }
+  };
+
+  this.highlightLastBar = () => this.highlightBar(barsNodes.length - 1);
+
+  this.highlightIndustriesForever = industries => {
+    industryChanges.forEach(({ industry }, i) => {
+      if (industries.includes(industry)) this.highlightBar(i, true);
+    });
+  };
+
+  // Horizontal scroll callback to highlight middle bar and stick axis
+  this.onScroll = scrollDistance => {
+    this.highlightBar(Math.round(scrollDistance / barWidth));
+  };
+
+  // Parses data-industries from paragraph blocks
+  let paragraphs;
+
+  function generateParagraphs() {
+    return Array.from(
+      document.querySelectorAll('#industry-content p'),
+      p => ({
+        node: p,
+        industries: p.getAttribute('data-industries').split(/;\s?/),
+      }),
     );
-    node.style.left =
-      Math.min(
-        Math.max(xAvg - node.clientWidth / 2, 20),
-        width - node.clientWidth - 20,
-      ) + 'px';
-    node.style.top = pctAvg < -0.2 ? yScale(0.45) : yScale(-0.3) + 'px';
-    highlightIndustriesForever(industries);
-  });
+  }
+
+  this.positionText = function () {
+    (paragraphs || (paragraphs = generateParagraphs())).forEach(
+      ({ node, industries }) => {
+        const xAvg = industries.reduce(
+          (avg, industry) => avg + xScale(industry) / industries.length,
+          0,
+        );
+        const pctAvg = industries.reduce(
+          (avg, industry) =>
+            avg + industryChangesRaw[industry] / industries.length,
+          0,
+        );
+        node.style.left =
+          Math.min(
+            Math.max(xAvg - node.clientWidth / 2, 20),
+            width - node.clientWidth - 20,
+          ) + 'px';
+        node.style.top = pctAvg < -0.2 ? yScale(0.45) : yScale(-0.3) + 'px';
+        this.highlightIndustriesForever(industries);
+      },
+    );
+  };
+
+  // Used for smol version to highlight bars
+  this.generateAndHighlightIndustries = () => {
+    this.highlightIndustriesForever(
+      divContainer.node().getAttribute('data-industries').split(/;\s?/),
+    );
+  };
 }
 
 // Initialization function
 function init() {
-  updateGraph();
-  positionText();
+  const largeContainer = select('#industry-impact-container');
+  const largeChart = new IndustryChart(largeContainer, true);
+  largeChart.updateGraph();
+  largeChart.positionText();
 
   scrollHorizontally({
-    container: divContainer,
+    container: largeContainer,
     padding: 'industry-impact-padding',
     topDetectorId: 'detect-graphic-top',
     bottomDetectorId: 'detect-graphic-bottom',
-    onScroll,
-    exitLeft: () => highlightBar(0),
-    exitRight: () => highlightBar(barsNodes.length - 1),
+    onScroll: largeChart.onScroll,
+    exitLeft: () => largeChart.highlightBar(0),
+    exitRight: () => largeChart.highlightLastBar(),
   });
+
+  const smallContainer = select('#smol-industry-impact');
+  const smallChart = new IndustryChart(smallContainer);
+  smallChart.updateGraph();
+  smallChart.generateAndHighlightIndustries();
+
+  window.addEventListener('resize', throttle(largeChart.updateGraph, 500));
 }
 
 init();
-
-window.addEventListener('resize', throttle(updateGraph, 500));
