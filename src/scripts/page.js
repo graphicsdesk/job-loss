e3c46c9f57e6b9d8ba4d019013eac6218c8a7814
Jason Kao <jason.kao@columbia.edu>
2020-05-14 23:31:08 -0400

    final touches

--- src/scripts/page.js
+++ src/scripts/page.js
@@ -25,7 +25,7 @@ const isOnContributorPage =
 
 const SECTION_MAIN_SELECTOR = 'section#main';
 const ARTICLE_SELECTOR =
-  '.pb-f-article-article-body > .row > .col-xs-12 > .ab-article-body > .ab-article-content > article';
+  '.pb-f-eye-article-article-body > .row > .col-xs-12 > .ab-article-body > .ab-article-content > article';
 
 // Replaces section#main with article
 function hoistArticle() {
